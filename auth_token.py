2c6f7282e168a5b5362b6f85cdade2b04fa5528a
sdupadhyay <sumit.upadhyay108@gmail.com>
2026-07-11 15:06:52 +0530

    Add JWT token creation and verification functions

---
+++ auth_token.py
@@ -0,0 +1,39 @@
+from jose import JWTError
+from fastapi import status
+from fastapi import HTTPException
+from time import timezone
+from datetime import datetime
+from datetime import timedelta
+from typing import Optional
+from jose import jwt
+
+SECRET_KEY = "12345678"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + (
+        expires_delta if expires_delta else timedelta(minutes=15)
+    )
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, key=SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_token(token: str):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    # Return Payload present in the token
+    return username
