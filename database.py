49b540120b655b41ffc7848e81486df2d017e863
sdupadhyay <sumit.upadhyay108@gmail.com>
2026-07-11 12:52:16 +0530

    Fix: correct spelling of database filename in connection string

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./spen_wise.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./spendwise.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
